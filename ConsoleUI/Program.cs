f68f2173c613f66011e0ccfae754e933fc5cdca6
salihsenturk97 <salihsenturk97@gmail.com>
2022-01-17 16:37:55 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,39 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Data Transformation Object (DTO)
+            //ProductTest();
+            //IoC
+            //CategoryTest();
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+            foreach (var category in categoryManager.GelAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetProductDetails())
+            {
+                Console.WriteLine(product.ProductName +"/" + product.CategoryName);
+            }
+        }
+    }
+}
---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess
+{
+    //Generic constraint - kısıt
+    //class : referans tip olabilir.
+    //IEntity : IEntity olabilir veya IEntity implement eden bir nesne olabilir .
+    //new() dedik çünkü newlenebilir olsun istedik bu sayede IEntity yazılamaz 
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
